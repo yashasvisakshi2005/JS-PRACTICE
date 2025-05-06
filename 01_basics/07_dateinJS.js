d177ccaaf3d5d5e47ef57d00e6401fd0321ad029
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-06 04:13:33 +0000

    01_basic end

---
+++ 01_basics/07_dateinJS.js
@@ -0,0 +1,9 @@
+// const now = new Date();
+// console.log(now.toString());
+// console.log(now.toDateString());     
+// console.log(typeof(now));
+// console.log(now.toLocaleDateString());
+let createdate = new Date(2023,0,23,5,3);
+console.log(createdate.toLocaleString());
+console.log(Date.now());
+
