e22983762b6dc99e1fc23a255d1a1f8311b41ecc
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-06 02:11:21 +0000

    string manipulation

---
+++ 01_basics/05_stringinterpolation.js
@@ -0,0 +1,12 @@
+const name = "hitesh";
+const repoCount = 50
+console.log(`Hello!,my name is ${name} and my repocount is ${repoCount}`);
+const n1 = " priyamani";
+console.log(n1.length);
+console.log(n1.toUpperCase());
+console.log(n1.substring(1,4));
+console.log(n1.slice(-2,-4));
+console.log(n1.replace("mani","money"));
+console.log(n1.trim());
+const text = "apple,banana,guava";
+console.log(text.split(","));
