3e63f6982021a16efb8dae9d408ec4d068d1ad65
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-15 04:36:24 +0000

    last lesson on array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,36 @@
+const myera = ["doraemon","ninja hattori","oggy","mickey mouse"]
+
+const didiera = ["oswald","thomas and friends","mr.bean","shaktimaan"]
+
+// const all_cartoon = myera.concat(didiera);
+// console.log(all_cartoon);
+
+// WE HAVE USED SPREAD OPERATOR HERE:-
+const all_cartoons = [...myera,...didiera]
+console.log(all_cartoons);
+
+// FLAT METHOD:-
+const arr_2 =[1,2,3,[4,5,[6,7,[8]]]]
+const arr_2_simplified = arr_2.flat(3);
+console.log(arr_2_simplified);
+
+// TO CHECK WHETHER GIVEN DATA IS ARRAY OR NOT
+console.log(Array.isArray("RESHU"));
+
+// TO TRANSFORM TO ARRAY M1
+console.log(Array.from("RESHU"));
+// TO TRANSFORM TO ARRAY M2
+const s1 = 100;
+const s2 = 200;
+const s3 = 300;
+console.log(Array.of(s1,s2,s3));
+
+//INTERESTING CASE
+
+console.log(Array.from({name:"YASHASVI"}));
+// TO GET BOTH
+console.log(Object.entries({name:"Yashasvi"}));
+//TO GET KEY
+console.log(Object.keys({name:"Yashasvi"}));
+//TO GET VALUES
+console.log(Object.values({name:"Yashasvi"}));
