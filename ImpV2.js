08c684c5e715a9a65de0686fef4e73d768f806b2
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-06-19 18:18:06 +0000

    JS learnings

--- ImpV2.js
+++ ImpV2.js
@@ -1,1 +1,7 @@
 //Q1
+function is_array(inp){
+    return inp instanceof Array;
+}
+console.log(is_array('w3school'));
+console.log(is_array([1,2,4,5]));
+//Q2
---
+++ Loops and String/loops.js
@@ -0,0 +1,26 @@
+//Q1
+for(let i=1;i<=10;i++){
+console.log(i);
+}
+//Q2
+for(let j=5;j>=1;j--){
+    console.log(j);
+}
+//Q3
+for(let j=1;j<=6;j++){
+    if(j==3){
+      continue;
+    }
+    else{
+        console.log(j);
+    }
+    // console.log(j);
+}
+//Q4
+let firstname = 'Shourya';
+let secondname = 'Sakshi';
+console.log(typeof firstname);
+let name =
+`My name
+is Yashasvi`
+console.log(name)
