ba57802d476085a256fcac42568d420c5313ae7f
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-07-07 15:43:05 +0000

    arrays learning

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,27 +1,92 @@
-const arr = [1,2,3,4,5];
-const myheros = ["shaktimaan","balveer","chhota bheem","mighty raju"];
-console.log(myheros[3]);
-//array methods
-//to add element into array
-arr.push(6);
-arr.push(7);
-// to remove last element of array
-arr.pop();
-// to add elemnt in beginning
-arr.unshift(9);
-//to remove firts element from array,no need to state any number
-arr.shift(); 
-console.log(arr);
-console.log(arr.includes(6));
-console.log(arr.indexOf(9));
+// const arr = [1,2,3,4,5];
+// const myheros = ["shaktimaan","balveer","chhota bheem","mighty raju"];
+// console.log(myheros[3]);
+// //array methods
+// //to add element into array
+// arr.push(6);
+// arr.push(7);
+// // to remove last element of array
+// arr.pop();
+// // to add elemnt in beginning
+// arr.unshift(9);
+// //to remove firts element from array,no need to state any number
+// arr.shift(); 
+// console.log(arr);
+// console.log(arr.includes(6));
+// console.log(arr.indexOf(9));
 
-//love babbar:-
-let obj ={
-    Name:"Yashasvi",
-    age:20,
-    weight: 70,
-    height:"5ft 7in",
-    greet:function(){
-        console.log("hi");
-    }
-}
+// //love babbar:-
+// let obj ={
+//     Name:"Yashasvi",
+//     age:20,
+//     weight: 70,
+//     height:"5ft 7in",
+//     greet:function(){
+//         console.log("hi");
+//     }
+// }
+let arr= [10,20,30,40];
+console.log(arr[0]);
+arr.push(60);
+console.log(arr[4]);
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compile
+// let arr =[10,30,20,50,10];
+// let newarr = arr.filter(function(val){
+//     if(val>20) return true; 
+// })
+
+// console.log(newarr);
+//  Q1
+// let fruits = ["apple","banana","guava"];
+// console.log(fruits[1]);
+//Q2
+// fruits.push("Mango");
+// fruits.unshift("Pineapple");
+// console.log(fruits);
+// fruits.replace("banana","kiwi");
+// fruits.pop();
+// console.log(fruits);
+// let colors =["green","yellow"];
+// colors.splice(1,0,"red","blue");
+// console.log(colors);
+// let names = ["zara","mira","bhavya","arjun"];
+// names.sort();
+// console.log(names);
+let arr = [34,56,78,90,60];
+// let sq = arr.map(function(val){
+//     return val*val;
+// });
+// console.log(sq);
+// let greater = arr.filter(function(val){
+//     if(val>50) return true;
+// })
+// console.log(greater);
+// let sum = arr.reduce(function(accumulator,val){
+//     return accumulator+val;
+// })
+// console.log(sum);
+// let find = arr.find(function(val){
+//     return val>10 
+// });
+// let some = arr.some(function(val){
+//     return val<35 ;
+// })
+// console.log(some);
+// let every = arr.every(function(val){
+//     return val%2==0;
+// })
+// console.log(every);
+// let fullname = ["yashasvi","sakshi"];
+// let[firstname,lastname]=fullname;
+// console.log(firstname);
+let arr1 = ["golu","kukki","shreya","reshu"];
+let arr2 = ["ishu",...arr1];
+
+// arr2.push("ishu");
+// let arr3 = ["mohit","pragya","sonu"]
+// let arr4 = [...arr2,...arr3]
+console.log(arr2);
+
+
+
